49075c8bf694bc53c2ee54130c0f77cbb4a9a9cb
wizard chiboy <doncodesacademy@gmail.com>
2022-01-20 01:28:12 +0100

    make it better

--- server.js
+++ server.js
@@ -1,103 +1,96 @@
 // imports
 
-import express from "express"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import Messages from "./dbMessages.js";
 import Pusher from "pusher";
+import cors from "cors";
 
 dotenv.config();
 
 // realtime configuration
-
-
 const pusher = new Pusher({
-    appId: "1332472",
-    key: process.env.pusher_key,
-    secret: process.env.pusher_secret,
-    cluster: "eu",
-    useTLS: true
-  });
-
-
+  appId: "1332472",
+  key: process.env.pusher_key,
+  secret: process.env.pusher_secret,
+  cluster: "eu",
+  useTLS: true,
+});
 
 // app configs
 
 const app = express();
-const port = process.env.PORT || 9999 
+const port = process.env.PORT || 9999;
 
 // middlewares
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 // Db config
 
-const mongo_url = process.env.MONGO_URL
+const mongo_url = process.env.MONGO_URL;
 mongoose.connect(mongo_url, {
-    useNewUrlParser: true,
-    useUnifiedTopology :true
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // db connection
-const db = mongoose.connection
+const db = mongoose.connection;
 
- db.once("open", () => {
-    console.log("connected to mongo and we are set!!")
+db.once("open", () => {
+  console.log("connected to mongo and we are set!!");
 
-    const msgCollection = db.collection("messagecontents")
-    const changeStream = msgCollection.watch()
+  const msgCollection = db.collection("messagecontents");
+  const changeStream = msgCollection.watch();
 
-    changeStream.on("change", (change) => {
-        console.log(change)
+  changeStream.on("change", (change) => {
+    console.log(change);
 
-        if(change.operationType === "insert") {
-
-        }
-
-    })
-})
-
-
-
-
+    if (change.operationType === "insert") {
+      const messageDetails = change.fullDocument;
+      pusher.trigger("messages", "inserted", {
+        name: messageDetails.name,
+        message: messageDetails.message,
+        timestamp: messageDetails.timestamp,
+        received: messageDetails.received,
+      });
+    } else {
+      console.log("error trigerring pusher");
+    }
+  });
+});
 
 // api endpoint
 
-app.get("/", (req,res)=> res.status(200).send("hello don")) 
+app.get("/", (req, res) => res.status(200).send("hello don"));
 
 // getting all data from db
 
 app.get("/messages/sync", (req, res) => {
-    Messages.find((err, data)=> {
-
-        if(err) {
-            res.status(err).send (err)
-        }
-
-        else {
-            res.status(200).send(data)
-        }
-
-    })
-})
-
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
 
 // posting a data to db
-app.post("/messages/new", (req,res) => {
-   const dbMessage = req.body
+app.post("/messages/new", (req, res) => {
+  const dbMessage = req.body;
 
-    Messages.create(dbMessage, (err, data) => {
-        if(err) {
-            res.status(500).send(err)
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
 
-        }
+// port listen
 
-        else {
-            res.status(201).send(data)
-        }
-    })
-})
-
-// listen
-
-app.listen(port , console.log(`hey am connected to port: ${port}` )) 
+app.listen(port, console.log(`hey am connected to port: ${port}`));
